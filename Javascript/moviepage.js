a6d459e727e03f97f1047b7081401c6dfd073056
Tirtha Jyoti Bol <83816482+TirthaJyotiBol@users.noreply.github.com>
2024-01-19 01:10:28 +0530

    check commit

--- Javascript/moviepage.js
+++ Javascript/moviepage.js
@@ -10,10 +10,13 @@ let genre = document.querySelector('#genre');
 let movieDirectors = document.querySelector('#directors');
 let movieRating = document.querySelector('#rating');
 let movieLanguage = document.querySelector('#language');
+let section = document.querySelector('section');
 
 
 function showDetails(id){
     let request = fetch(`https://www.omdbapi.com/?i=${id}&apikey=${apiKey}`);
+    let ratingBox_container = document.querySelector('#rating-box-container');
+
     request.then((res)=>{
         return res.json();
     })
@@ -26,6 +29,18 @@ function showDetails(id){
         movieDirectors.textContent = data.Director;
         movieRating.textContent = data.imdbRating;
         movieLanguage.textContent = data.Language;
+
+        let ratingArray = data.Ratings;
+        ratingArray.forEach((curr)=>{
+            let tag = `
+            <div class="rating-box">
+                <h3 class="rated-by">${curr.Source}</h3>
+                <p class="rated-value">${curr.Value}</p>
+            </div>
+            `
+            ratingBox_container.insertAdjacentHTML("afterbegin",tag);
+        })
+
     })
 }
 
